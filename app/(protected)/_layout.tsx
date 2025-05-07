a4c7dceb880c1ae9e5ad31f6d0344cfad6710d1f
Yassine ANZAR BASHA <yassanz.contact@gmail.com>
2025-05-07 16:59:10 +0200

    fix: remove accommodation check

--- app/(protected)/_layout.tsx
+++ app/(protected)/_layout.tsx
@@ -1,14 +1,10 @@
 import { Stack } from "expo-router";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { getUserByAccessToken } from "@/services/utils";
-import { UserResponse } from "@/types/zod/user";
+
 import { Alert } from "react-native";
-import { AuthService } from "@/services/server/auth";
 
 export default function ProtectedLayout() {
-  const [user, setUser] = useState<UserResponse | null>(null);
-  const authService = new AuthService();
-
   useEffect(() => {
     const checkAuth = async () => {
       try {
@@ -16,10 +12,6 @@ export default function ProtectedLayout() {
         if (!userData) {
           return;
         }
-        if (userData && !userData.accommodationId) {
-          return await authService.logout(); // renvoyer vers la page de création d'accommodation
-        }
-        setUser(userData);
       } catch (error: any) {
         Alert.alert(error.message);
       }
