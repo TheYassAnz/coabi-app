4295513579480ff2e44808a626145abc75b95df1
Yassine ANZAR BASHA <yassanz.contact@gmail.com>
2025-05-07 16:27:31 +0200

    feat: remove JoinPage component and add new UI components

--- app/(protected)/(tabs)/_layout.tsx
+++ app/(protected)/(tabs)/_layout.tsx
@@ -1,16 +1,30 @@
-import { Tabs } from "expo-router";
-
+import { Tabs, useRouter } from "expo-router";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import { useEffect, useState } from "react";
-import { UserService } from "@/services/server/user";
-import { getUserById } from "@/services/utils";
+import { getUserByAccessToken } from "@/services/utils";
 
 export default function TabLayout() {
-  const userService = new UserService();
-  const [isJoinedAccommodation, setIsJoinedAccommodation] = useState(false);
+  const router = useRouter();
+  const [isJoinedAccommodation, setIsJoinedAccommodation] = useState<
+    boolean | null
+  >(null);
   useEffect(() => {
-    // TO DO
+    getUserByAccessToken()
+      .then((user: any) => {
+        if (user.accommodationId) {
+          setIsJoinedAccommodation(true);
+        } else {
+          router.replace("/accommodation/join");
+        }
+      })
+      .catch(() => {
+        setIsJoinedAccommodation(false);
+      });
   }, []);
+
+  if (isJoinedAccommodation === null) {
+    return null;
+  }
 
   return (
     <Tabs
