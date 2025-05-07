450491220f90d4682869082e4b6a9b617800aec7
Quentin <quentinucme@hotmail.fr>
2025-05-07 16:40:29 +0200

    feat: initialize refund page

--- app/(protected)/(tabs)/refund.tsx
+++ app/(protected)/(tabs)/refund.tsx
@@ -1,9 +1,33 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Pre } from "@expo/html-elements";
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  Pressable,
+  Modal,
+} from "react-native";
 
 export default function RefundScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Refund screen</Text>
+      <View style={styles.firstcontainer}>
+        <Text style={styles.text}>Refund</Text>
+      </View>
+      <Text style={styles.text}>Ecran du refund</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="10€"
+        keyboardType="numeric"
+      />
+      <Pressable
+        style={styles.button}
+        onPress={() => {
+          console.log("Refund created");
+        }}
+      >
+        <Text>Create a refund</Text>
+      </Pressable>
     </View>
   );
 }
@@ -11,10 +35,27 @@ export default function RefundScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+  },
+  firstcontainer: {
+    backgroundColor: "red",
+    flex: 0.3,
   },
   text: {
     color: "#000000",
+  },
+  input: {
+    height: 40,
+    borderColor: "#ccc",
+    borderWidth: 1,
+    padding: 10,
+    width: "50%",
+    marginTop: 10,
+    borderRadius: 5,
+  },
+  button: {
+    backgroundColor: "#007BFF",
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 20,
   },
 });
